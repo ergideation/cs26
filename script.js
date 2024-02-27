dbc217415d84ac64b5ea728b551693d7190ea81a
ergideation <160720555+ergideation@users.noreply.github.com>
2024-02-28 03:17:40 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.getElementById('no-btn').addEventListener('click', function() {
     document.getElementById('invitation-text').innerText = "Please think again";
     this.style.padding = "5px 10px"; // Make "No" button smaller
     document.getElementById('yes-btn').style.padding = "15px 30px"; // Make "Yes" button bigger
-    document.getElementById('event-image').src = "first.png"; // Change the image source if needed
+    document.getElementById('event-image').src = "first.jpg"; // Change the image source if needed
 });
