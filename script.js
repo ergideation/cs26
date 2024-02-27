86578e4c58b1a297ddeb73a98fae97fdf11ca2f3
ergideation <160720555+ergideation@users.noreply.github.com>
2024-02-28 03:52:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+let noButtonClickCount = 0;
 document.getElementById('no-btn').addEventListener('click', function() {
+		noButtonClickCount++;
+    if (noButtonClickCount === 1) {
     document.getElementById('invitation-text').innerText = "Please think again";
-    this.style.padding = "5px 10px"; // Make "No" button smaller
-    document.getElementById('yes-btn').style.padding = "15px 30px"; // Make "Yes" button bigger
+    this.style.padding = "10px 40px"; // Make "No" button smaller
+    document.getElementById('yes-btn').style.padding = "15px 40px"; // Make "Yes" button bigger
     document.getElementById('event-image').src = "second.jpg"; // Change the image source if needed
+    }
+    else if(noButtonClickCount > 1){
+    document.getElementById('invitation-text').innerText = "You will now be banned from all cs chats...";
+    this.style.padding = "7px 30px"; // Make "No" button smaller
+    document.getElementById('yes-btn').style.padding = "20px 50px"; // Make "Yes" button bigger
+    document.getElementById('event-image').src = "third.png"; // Change the image source if needed
+    }
 });
+
+
+document.getElementById('yes-btn').addEventListener('click', function() {
+    document.getElementById('invitation-text').innerText = "Yaaaay!";
+    //document.getElementById('event-image').src = "yes.png"; // Change the image source if needed
+     document.getElementById('event-image').style.display = "none"; // Hide the static image
+    document.getElementById('gif-container').style.display = "block"; // Show the GIF embed
+});
+
